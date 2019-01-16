157d099adbca4f533570ef4a274dcb6c786f03f4
Calico Randall <calicorrandall@gmail.com>
2019-01-16 15:06:06 -0800

    initialfastandsteep

---
+++ FastandSteep.js
@@ -0,0 +1,34 @@
+function setup(){
+  createCanvas(600,600);
+  background(0);
+}
+
+function draw(){
+  stroke(255,0,0);
+  fill(255);
+  triangle(275,0,600,300,275,600);
+  
+  stroke(255,0,0);
+  fill(0);
+  triangle(50,300,225,200,495,395);
+  stroke(255);
+  triangle(60,290,230,205,490,415);
+  stroke(255,0,0);
+  triangle(70,280,235,210,485,435);
+  stroke(255);
+  triangle(80,270,240,215,480,455);
+  stroke(255,0,0);
+  triangle(90,260,245,220,475,475);
+  stroke(255);
+  triangle(100,250,250,225,470,495);
+  stroke(255,0,0);
+  triangle(110,240,255,230,465,515);
+  stroke(255);
+  triangle(120,230,260,235,460,535);
+  stroke(255,0,0);
+  triangle(130,220,265,240,455,555);
+  stroke(255);
+  triangle(140,210,270,245,450,575);
+  stroke(255,0,0);
+  triangle(150,200,275,250,445,595);
+  }
